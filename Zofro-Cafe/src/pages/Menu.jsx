8dd54dca60463d3362503fa8ef7ba82de5b717f8
Anie <anie.jerson.25@gmail.com>
2026-05-20 02:03:39 +0530

    Refactor Menu component: extract MenuBar into its own component and remove unused code

--- Zofro-Cafe/src/pages/Menu.jsx
+++ Zofro-Cafe/src/pages/Menu.jsx
@@ -1,64 +1,13 @@
 import React, { useState } from "react";
-import Favorite from "../components/Favorite";
+import MenuBar from "../components/MenuBar";
+
 import "./menu.css";
 
-import { FaRegHeart } from "react-icons/fa";
-import { IoSearch } from "react-icons/io5";
-import { CiCoffeeCup } from "react-icons/ci";
 
 const Menu = () => {
-  const [activeButton, setActiveButton] = useState("");
-
   return (
-    <div className="menu-page">
-      {/* TOP MENU BAR */}
-      <div className="menu-top">
-        {activeButton === "search" ? (
-          <div className="search-container">
-            <input type="text" placeholder="Search coffee..." />
-
-            <button className="search-btn" onClick={() => setActiveButton(null)}>
-              Search
-            </button>
-          </div>
-        ) : (
-          <>
-          {/* FAVORITE */}
-            <button
-              className={
-                activeButton === "favorite"
-                  ? "menu-btn active-menu-btn"
-                  : "menu-btn"
-              }
-              onClick={() => setActiveButton("favorite")}
-            >
-              <FaRegHeart />
-            </button>
-
-            {/* SEARCH */}
-            <button
-              className="menu-btn"
-              onClick={() => setActiveButton("search")}
-            >
-              <IoSearch />
-            </button>
-
-            {/* COFFEE */}
-            <button
-              className={
-                activeButton === "coffee"
-                  ? "menu-btn active-menu-btn"
-                  : "menu-btn"
-              }
-              onClick={() => setActiveButton("coffee")}
-            >
-              <CiCoffeeCup />
-            </button>
-          </>
-        )}
-      </div>
-      {/* SEARCH PAGE */}
-      {activeButton === "favorite" && <Favorite />}
+    <div className="menu">
+      <MenuBar />
     </div>
   );
 };
